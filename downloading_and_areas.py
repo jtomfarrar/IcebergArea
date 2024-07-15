bb1310a900324f039b11daadb2720e0f301bda41
elligonzalez <eliana.gonzalez@noaa.gov>
2024-07-15 14:26:19 -0400

    Update downloading_and_areas.py

--- downloading_and_areas.py
+++ downloading_and_areas.py
@@ -1,7 +1,3 @@
-'''
-Includes download_and_areas. downloadCSV and pixel_area are called from another file. They are on github elligonzalez/IcebergArea.
-'''
-
 import ice
 from pixel_area import pixel_area
 from download_data import tiffs
@@ -10,26 +6,47 @@ import os
 import rasterio
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-  
-def download_and_areas(icebergname, output_location, coord1 = 'lat', coord2 = 'lon', timeRange=None,layerName=None):
-  '''Function to download all NASA satellite images of an iceberg recorded from BYU and find it's daily area from the images
-     Args:
+def download_and_areas(icebergname, output_location, coord1 = 'lat', coord2 = 'lon', timeRange=None):
+    '''Function to download all NASA satelite images of an iceberg recorded from BYU and find it's daily area from the images
+        Args:
            icebergname (string): iceberg name from BYU data
            output_location (string): output path
            coord1 (string): name of latitude column, optional if using BYU Stats Database
            coord2 (string): name of longitude column, optional if using BYU Stats Database
            timeRange (tuple): for select time ranges in csv file, optional
-           layerName (string): for isolating bands, optional
-      Returns:
-            Dictionary of daily areas from non-cloudy, non-cropped images of the iceberg from NASA.
-  '''
-  downloadCSV('Stats Database/'+icebergname+'.csv', output_location, coord1, coord2,timeRange)
-  directory = output_location
-  areas_dict = {}
-  for image in os.listdir(directory):
-      name = image.rstrip('.tif')
-      area = pixel_area(directory+image,layerName)
-      areas_dict[str(name)] = area
-  print(areas_dict)
-  return areas_dict
+        Returns: Dictionary of daily areas from non-cloudy, non-cropped images of the iceberg from NASA. Basic scatter plot of data.'''
+    downloadCSV('Stats Database/'+icebergname+'.csv', output_location, coord1, coord2,timeRange)
+    directory = output_location
+    areas_dict = {}
+    for image in os.listdir(directory):
+        name = image.rstrip('.tif')
+        area = pixel_area(directory+image)
+        areas_dict[str(name)] = area
+
+    dates = []
+    data = []
+    for date in areas_dict:
+        if type(areas_dict[date]) == float:
+            dates.append(date)
+            data.append(areas_dict[date])
+        else:
+            pass
+
+    date = pd.to_datetime(dates)
+    DF = pd.DataFrame()
+    DF['value'] = data
+    DF = DF.set_index(date)
+    plt.scatter(DF.index, DF['value'], color='blue', s=16)
+    plt.gcf().autofmt_xdate()
+    
+    
+    plt.xlabel('Date')
+    plt.ylabel('Area km2')
+     
+    plt.title(f'{icebergname} Area')
+     
+    plt.show()
+    return areas_dict
