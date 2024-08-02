9c2a18819d86a93b7ffba6b395bf72c2c157d530
elligonzalez <eliana.gonzalez@noaa.gov>
2024-08-02 10:21:06 -0400

    Update pixelarea_center.py

--- pixelarea_center.py
+++ pixelarea_center.py
@@ -1,3 +1,7 @@
+'''
+Finds the area of an iceberg, where the iceberg is located in the center of the image.
+If it's not containing the center coordinate, it finds the largest area.
+'''
 import rasterio
 from rasterio.plot import show
 import sys
@@ -88,133 +92,95 @@ def find_interior(mask,directory):
     interior_coordinates = coordinates[:, ::-1]
 
     dataset = rasterio.open(directory)
-    xmin = dataset.bounds[0]
-    ymin = dataset.bounds[1]
-    xmax = dataset.bounds[2]
-    ymax = dataset.bounds[3]
-    width = dataset.width
-    height = dataset.height
-    total_pixel = width*height
 
     coordinates = []
     for point in interior_coordinates:
-        x, y = pixel_to_coordinates(point[0], point[1], width, height, xmin, ymin, xmax, ymax)
-        coordinates.append((x, y))
+        coordinate = dataset.transform*point
+        coordinates.append(coordinate)
     return coordinates
 
-
-def pixel_to_coordinates(x, y, width, height, xmin, ymin, xmax, ymax):
-    ''' Function to convert pixels to coordinates.
-        Args: 
-                x (float): x pixel coordinate
-                y (float): y pixel coordinate
-                width (float): width of the image in the number of pixels, taken from the dataset
-                height (float): height of the image in the number of pixels, taken from the dataset
-                xmin (float): lower bound (left) of the x coordinates of the image, taken from the dataset
-                ymin (float): lower bound (bottom) of the y coordinates of the image, taken from the dataset
-                xmax (float): upper bound (right) of the x coordinates of the image, taken from the dataset
-                ymax (float): upper bound (top) of the y coordinates of the image, taken from the dataset
-        Returns:
-                Converted x,y coordinates to lon, lat coordinates
-    ''' 
-    x_ratio = x / width
-    y_ratio = y / height
-
-    x = xmin + (xmax - xmin) * x_ratio
-    y = ymin + (ymax - ymin) * y_ratio
-    
-    return x, y
 
 
 def pixel_area(mask,directory):
 
-    try:
                 
-        #Finding the interior coordinates of the contour, using mask from ice_olate    
-        mask_binary = (mask > 0).astype(np.uint8)
-        coordinates = np.column_stack(np.where(mask_binary))
-        interior_coordinates = coordinates[:, ::-1]  
+    #Finding the interior coordinates of the contour, using mask from ice_olate    
+    mask_binary = (mask > 0).astype(np.uint8)
+    coordinates = np.column_stack(np.where(mask_binary))
+    interior_coordinates = coordinates[:, ::-1]  
+
+    #Finding data from image    
+    dataset = rasterio.open(directory)
+    width = dataset.width
+    height = dataset.height
+    total_pixel = width*height
     
-        #Finding data from image    
-        dataset = rasterio.open(directory)
-        xmin = dataset.bounds[0]
-        ymin = dataset.bounds[1]
-        xmax = dataset.bounds[2]
-        ymax = dataset.bounds[3]
-        width = dataset.width
-        height = dataset.height
-        total_pixel = width*height
+    # Creates an upper limit to how big the iceberg can be, or the mask of the iceberg. Edit percentage to the expected iceberg limit.
+    nonZero = cv2.countNonZero(mask) #count nonzero pixels
+    if nonZero >= .1*total_pixel:
+            print('Too big.')
+            return('Too big.')
+    
+    # Convert pixel coordinates to polar stereographic coordinates
+    coordinates = []
+    for point in interior_coordinates:
+        coordinate = dataset.transform*point
+        coordinates.append(coordinate)
+
+    #Make a list of all the coordinates. In each [[all four coordinates for one center],[all four coordinates for the next center]...etc]
+    res = dataset.res
+    corners_coordinates = []
+    for coordinate in coordinates:
+        individual_corner = []
+        corner1 = (coordinate[0] - res[0]/2, coordinate[1] + res[1]/2)
+        corner2 = (coordinate[0] + res[0]/2, coordinate[1] + res[1]/2)
+        corner3 = (coordinate[0] + res[0]/2, coordinate[1] - res[1]/2)
+        corner4 = (coordinate[0] - res[0]/2, coordinate[1] - res[1]/2)
+        individual_corner.append(corner1)
+        individual_corner.append(corner2)
+        individual_corner.append(corner3)
+        individual_corner.append(corner4)
+        corners_coordinates.append(individual_corner)
+
+
+    #Transform corners in PS to Lat/Lon
+    transformer = Transformer.from_crs("epsg:3031", "epsg:4326") 
+    latlon_corners = []
+    for coordinate in corners_coordinates:
+        individual_corner = []
+        onei,onej = transformer.transform(coordinate[0][0],coordinate[0][1])
+        twoi,twoj = transformer.transform(coordinate[1][0],coordinate[1][1])
+        threei,threej = transformer.transform(coordinate[2][0],coordinate[2][1])
+        fouri,fourj = transformer.transform(coordinate[3][0],coordinate[3][1])
+        individual_corner.append([onei,onej])
+        individual_corner.append([twoi,twoj])
+        individual_corner.append([threei,threej])
+        individual_corner.append([fouri,fourj])
+        latlon_corners.append(individual_corner)
+
+    #Finding the area in m2    
+    total_area = 0
+    for coordinate in latlon_corners:
+        max_lat = max(coordinate[0][0],coordinate[1][0],coordinate[2][0],coordinate[3][0])
+        min_lat = min(coordinate[0][0],coordinate[1][0],coordinate[2][0],coordinate[3][0])
+        max_lon = max(coordinate[0][1],coordinate[1][1],coordinate[2][1],coordinate[3][1])
+        min_lon = min(coordinate[0][1],coordinate[1][1],coordinate[2][1],coordinate[3][1])
+    
+        pixel = {"type": "Polygon", "coordinates": [
+            [(coordinate[0][1],coordinate[0][0]),
+             (coordinate[1][1],coordinate[1][0]),
+             (coordinate[2][1],coordinate[2][0]),
+             (coordinate[3][1],coordinate[3][0]),
+             (coordinate[0][1],coordinate[0][0])]]}
+        lon, lat = zip(*pixel['coordinates'][0])
+        pa = Proj('+proj=aea +lat_1=' + str(min_lat) + ' +lat_2=' + str(max_lat) + ' +lat_0=' + str((max_lat-min_lat)/2) + ' +lon_0=' + str((max_lon-min_lon)/2))
+        x, y = pa(lon, lat)
+        cop = {"type": "Polygon", "coordinates": [zip(x, y)]}
+        total_area += shape(cop).area
         
-        # Creates an upper limit to how big the iceberg can be, or the mask of the iceberg. Edit percentage to the expected iceberg limit.
-        nonZero = cv2.countNonZero(mask) #count nonzero pixels
-        if nonZero >= .1*total_pixel:
-                print('Too big.')
-                return('Too big.')
-        
-        # Convert pixel coordinates to polar stereographic coordinates
-        coordinates = []
-        for point in interior_coordinates:
-            x, y = pixel_to_coordinates(point[0], point[1], width, height, xmin, ymin, xmax, ymax)
-            coordinates.append((x, y))
-    
-        #Make a list of all the coordinates. In each [[all four coordinates for one center],[all four coordinates for the next center]...etc]
-        dataset = rasterio.open(directory)
-        res = dataset.res
-        corners_coordinates = []
-        for coordinate in coordinates:
-            individual_corner = []
-            corner1 = (coordinate[0] - res[0]/2, coordinate[1] + res[1]/2)
-            corner2 = (coordinate[0] + res[0]/2, coordinate[1] + res[1]/2)
-            corner3 = (coordinate[0] + res[0]/2, coordinate[1] - res[1]/2)
-            corner4 = (coordinate[0] - res[0]/2, coordinate[1] - res[1]/2)
-            individual_corner.append(corner1)
-            individual_corner.append(corner2)
-            individual_corner.append(corner3)
-            individual_corner.append(corner4)
-            corners_coordinates.append(individual_corner)
-    
-    
-        #Transform corners in PS to Lat/Lon
-        transformer = Transformer.from_crs("epsg:3031", "epsg:4326") 
-        latlon_corners = []
-        for coordinate in corners_coordinates:
-            individual_corner = []
-            onei,onej = transformer.transform(coordinate[0][0],coordinate[0][1])
-            twoi,twoj = transformer.transform(coordinate[1][0],coordinate[1][1])
-            threei,threej = transformer.transform(coordinate[2][0],coordinate[2][1])
-            fouri,fourj = transformer.transform(coordinate[3][0],coordinate[3][1])
-            individual_corner.append([onei,onej])
-            individual_corner.append([twoi,twoj])
-            individual_corner.append([threei,threej])
-            individual_corner.append([fouri,fourj])
-            latlon_corners.append(individual_corner)
-    
-        #Finding the area in m2    
-        total_area = 0
-        for coordinate in latlon_corners:
-            max_lat = max(coordinate[0][0],coordinate[1][0],coordinate[2][0],coordinate[3][0])
-            min_lat = min(coordinate[0][0],coordinate[1][0],coordinate[2][0],coordinate[3][0])
-            max_lon = max(coordinate[0][1],coordinate[1][1],coordinate[2][1],coordinate[3][1])
-            min_lon = min(coordinate[0][1],coordinate[1][1],coordinate[2][1],coordinate[3][1])
-        
-            pixel = {"type": "Polygon", "coordinates": [
-                [(coordinate[0][1],coordinate[0][0]),
-                 (coordinate[1][1],coordinate[1][0]),
-                 (coordinate[2][1],coordinate[2][0]),
-                 (coordinate[3][1],coordinate[3][0]),
-                 (coordinate[0][1],coordinate[0][0])]]}
-            lon, lat = zip(*pixel['coordinates'][0])
-            pa = Proj('+proj=aea +lat_1=' + str(min_lat) + ' +lat_2=' + str(max_lat) + ' +lat_0=' + str((max_lat-min_lat)/2) + ' +lon_0=' + str((max_lon-min_lon)/2))
-            x, y = pa(lon, lat)
-            cop = {"type": "Polygon", "coordinates": [zip(x, y)]}
-            total_area += shape(cop).area
-            
-        #Convert to km2
-        areakm = total_area/1e+6
-        return areakm
-        
-    except:
-        return('Too cloudy.')
+    #Convert to km2
+    areakm = total_area/1e+6
+    return areakm
 
     
     
@@ -309,7 +275,7 @@ def ice_olate(directory,layerName=None,display=False,setThresh=None):
                                     plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB)) #show image
                                     plt.show()
                                     print('AREA',BergArea) #estimated area
-
+    
                         else: #conotur is not the first one
                             h, w = img.shape[:2]
                             mask = np.zeros((h, w), np.uint8)
@@ -326,12 +292,28 @@ def ice_olate(directory,layerName=None,display=False,setThresh=None):
                                     plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
                                     plt.show()
                                     print('AREA',BergArea)
+                                    
+                    if BergArea == 'N/A':
+                        cnt = max(contours, key=cv2.contourArea) #find max contour
+                        h, w = img.shape[:2]
+                        mask = np.zeros((h, w), np.uint8)
+                        cv2.drawContours(mask, [cnt],-1, 255, -1)
+                        BergArea = pixel_area(mask,directory+file)
+                        if display == True: #display contour
+                            h, w = img.shape[:2]
+                            mask = np.zeros((h, w), np.uint8)
+                            cv2.drawContours(mask, [cnt],-1, 255, -1) #create mask
+                            res = cv2.bitwise_and(img, img, mask=mask) #apply mask to original image
+                            print('CONTOUR') #current contour
+                            plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB)) #show image
+                            plt.show()
+                            print('AREA',BergArea) #estimated area
                              
-
+    
                 try:
                     print('Final Area:',BergArea)
                     areas_dict[str(name)] = BergArea
-
+    
                 except:
                     print('Cropped')
     print(areas_dict)
